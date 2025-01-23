df42aec7b9f2cb05593e5969fe08a86a4f612087
Clément VAUCLARE <145541693+clementvclr@users.noreply.github.com>
2025-01-23 16:29:01 +0100

    Delete Gestion Stage directory

--- Gestion Stage/APPLICATION/getemployebyentreprise.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "connexion.php";
-
-    $uid = $_POST['id_row'];
-    $insertion= array();
-    $insertion = $connexion->query("SELECT * FROM entreprise WHERE idEntreprise = '$uid'")->fetch(PDO::FETCH_OBJ);
-    echo (json_encode($insertion));
-?>
--- Gestion Stage/APPLICATION/getemployebyetudiant.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "connexion.php";
-
-    $uid = $_POST['id_row'];
-    $insertion= array();
-    $insertion = $connexion->query("SELECT * FROM etudiant WHERE numEtudiant = '$uid'")->fetch(PDO::FETCH_OBJ);
-    echo (json_encode($insertion));
-?>
